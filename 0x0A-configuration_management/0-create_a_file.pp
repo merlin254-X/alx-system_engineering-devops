40a86bb7e3dfbdfe6da24d1dcd3f42bc6a1e456c
merlin254-X <merlinderick8@gmail.com>
2024-03-22 13:01:55 -0700

    create

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,12 +1,3 @@
-#create a file named school in the /tmp directory
-
-file { '/tmp/school':
-  ensure  => file,
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet',
-
 # Create a file in /tmp
 
 file { 'school':
