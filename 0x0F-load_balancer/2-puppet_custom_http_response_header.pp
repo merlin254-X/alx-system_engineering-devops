8037602a6d4585673a1999ae65ab85393e6df55d
merlin254-X <merlinderick8@gmail.com>
2024-04-04 02:21:49 -0700

    puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,49 +1,19 @@
 #!/usr/bin/env bash
-# Define a custom HTTP header response for Nginx
-class custom_http_response_header {
+# Define a custom HTTP header response for Nginx using puppet
 
-  # Install package 'augeas-tools' required for manipulating Nginx configuration
-  package { 'augeas-tools':
-    ensure => installed,
-  }
-
-  # Define custom HTTP header configuration
-  file { '/etc/nginx/sites-available/default':
-    ensure  => file,
-    content => "server {
-  listen 80 default_server;
-  listen [::]:80 default_server;
-
-  server_name _;
-
-  root /var/www/html;
-  index index.html index.htm;
-
-  location / {
-    try_files \$uri \$uri/ =404;
-  }
-
-  # Add custom HTTP header
-  add_header X-Served-By \$hostname;
-}",
-    notify  => Service['nginx'],
-  }
-
-  # Enable the custom HTTP header configuration
-  exec { 'enable_custom_http_header':
-    command => 'ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/',
-    unless  => 'test -L /etc/nginx/sites-enabled/default',
-    require => File['/etc/nginx/sites-available/default'],
-    notify  => Service['nginx'],
-  }
-
-  # Reload Nginx to apply changes
-  service { 'nginx':
-    ensure  => running,
-    enable  => true,
-    require => File['/etc/nginx/sites-available/default'],
-  }
+# install and configure nginx
+exec {'update':
+  command => '/usr/bin/apt-get update',
 }
-
-# Include the custom HTTP header configuration
-include custom_http_response_header
+-> package { 'nginx':
+  ensure => installed,
+}
+-> file_line { 'header_served_by':
+  path  => '/etc/nginx/sites-available/default',
+  match => '^server {',
+  line  => "server {\n\tadd_header X-Served-By \"${hostname}\";",
+  multiple => false,
+}
+-> exec {'run':
+  command => '/usr/sbin/service nginx restart',
+}
